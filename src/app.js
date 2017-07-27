f51798eca5d2d33b2bfa67e4125115b5e3905042
oggata <oggata@gmail.com>
2017-07-27 16:42:04 +0900

    first commit

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import isEmpty from 'lodash/isEmpty';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { StackNavigator } from 'react-navigation';
+import LoginScreen from './components/login';
+import Drawer from './components/drawer';
+
+const createStackNavigator = currentUser => StackNavigator({
+  LoginScreen: { screen: LoginScreen },
+  Drawer: { screen: Drawer },
+}, {
+  initialRouteName: isEmpty(currentUser) ? 'LoginScreen' : 'Drawer',
+});
+
+/*
+const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  CompanyDetail: { screen: CompanyDetailScreen },
+  Chart: { screen: ChartScreen },
+  SideMenuGroup : {screen : SideMenuScreen},
+  ListGroup : {screen : ListScreen},
+});
+*/
+
+class App extends Component {
+  render() {
+    const { currentUser } = this.props;
+    const Navigator = createStackNavigator(currentUser);
+    return (
+      <Navigator />
+    );
+  }
+}
+
+App.propTypes = {
+  currentUser: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = store => ({
+  currentUser: store.currentUser,
+});
+
+export default connect(mapStateToProps)(App);
